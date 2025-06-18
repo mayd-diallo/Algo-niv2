5b932e78d516843dc4f40fba7499c216ab83ce18
mayd-diallo <maydiallo78@gmail.com>
2025-06-18 12:32:25 +0200

    algo2

---
+++ Intro/IterationFOR.java
@@ -0,0 +1,23 @@
+package Intro;
+public class IterationFOR {
+
+
+        public static void main(String[] args) {
+
+            int i;
+
+            /*Compteur FOR */ 
+            for(i=10; i>=0; i--){
+
+                System.out.println(i);
+
+            }
+
+            i=0;
+
+
+        }
+
+        
+
+}
